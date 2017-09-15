ad327fea266d45efd006b495277a58a389e4dbb4
Oguzhan Gungor <ocg@aslan-apps.com>
2017-09-16 01:17:55 +1000

    init

---
+++ OCGXPathDictionary/OCGXPathDictionary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCGXPathDictionary
+//
+//  Created by Oguzhan Gungor on 16/9/17.
+//  Copyright © 2017 Aslan-Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
