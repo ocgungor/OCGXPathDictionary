012b5ec61e4f491b4fa63ebceb19b0f10faa7c64
Oguzhan Gungor <ocg@aslan-apps.com>
2017-09-16 23:37:25 +1000

    Adding categories

---
+++ OCGXPathDictionary/OCGXPathDictionary/Classes/OCGXPathDictionary.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+OCGXPathDictionary.h
+//  OCGXPathDictionary
+//
+//  Created by Oguzhan Gungor on 16/9/17.
+//  Copyright © 2017 Aslan-Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (OCGXPathDictionary)
+
+@end
---
+++ OCGXPathDictionary/OCGXPathDictionary/Classes/OCGXPathDictionary.m
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+OCGXPathDictionary.m
+//  OCGXPathDictionary
+//
+//  Created by Oguzhan Gungor on 16/9/17.
+//  Copyright © 2017 Aslan-Apps. All rights reserved.
+//
+
+#import "OCGXPathDictionary.h"
+
+@implementation NSDictionary (OCGXPathDictionary)
+
+@end
